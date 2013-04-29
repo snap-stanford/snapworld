7177be7c181b7a886c0907a974d697fb4726393d
Nikhil Khadke <nikhil.khadke@gmail.com>
2013-04-28 17:24:42 -0700

    perf fix

--- python/perf.py
+++ python/perf.py
@@ -7,15 +7,15 @@ class Timer(object):
         self.start_times = {}
         self.extras = {}
 
-    def start(self, tag=None, extra=None):
+    def start(self, tag, extra=None):
         self.start_times[tag] = time.time()
         self.extras[tag] = extra
 
-    def stop(self, tag=None):
+    def stop(self, tag):
         time_taken = time.time() - self.start_times[tag]
         extra = self.extras[tag]
         if extra is not None:
-            self.log.debug("[timer] [%s] (%s) %d ms" % (tag, extra, time_taken))
+            self.log.debug("[timer] [%s] (%s) %d ms" % (tag, str(extra), time_taken))
         else:
             self.log.debug("[timer] [%s] %d ms" % (tag, time_taken))
         del self.start_times[tag]
