b87daa41178d71287c760980a7c97ceb1add3393
Stephen Macke <smackee618@gmail.com>
2014-03-09 22:25:15 -0700

    revert personal changes to supervisors script

--- supervisor.sh
+++ supervisor.sh
@@ -20,7 +20,7 @@ mkdir -p $SNAPW_EXEC
 mkdir -p $SNAPW_OUTPUT
 
 ###############
-SRC=~/dev
+SRC=~/hanworks
 if [ -d  "$SRC" ]; then
     fs flushvolume -path ${SRC}
     cp ${SRC}/snapworld/python/* ${SNAPW_BIN}
