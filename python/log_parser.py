f0e3f7469ed0d158fa6f8cc894a1b2727c3cfc47
Mark Ulrich <mulrich@stanford.edu>
2014-04-08 14:47:02 -0700

    Fixed errant return

--- python/log_parser.py
+++ python/log_parser.py
@@ -358,7 +358,6 @@ def process_run(master_log_name, yperf_path, reset):
     times = get_step_timestamps(master_log_name)
     files = get_file_list(times)
     run_name = 'metrics-' + times[0].strftime('%Y%m%d-%H:%M:%S') # TODO os.path.split(os.path.dirname(master_log_name))[1]
-    return 'yperf-' + t.strftime('%Y%m%d-%H')
     yperf_path += run_name + '/'
     os.system('mkdir -p {0}'.format(yperf_path))
     json_path = WEB_DEPLOY_PATH + run_name + '/json/'
